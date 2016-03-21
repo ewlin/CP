5a4571a254a699f2f85f6b8a84f4bad0cb880f35
ewlin <ericwilliamlin@gmail.com>
2016-03-21 14:03:52 -0400

    cracklepop + new range func

---
+++ cracklePop.js
@@ -0,0 +1,37 @@
+function range (stop, start = 0, step = 1) {
+    let range = [],
+        currentVal = start;
+    
+    if (!stop) {
+        throw new Error('range must include a stop value!');
+    }
+    
+    while (currentVal < stop) {
+        range.push(currentVal);
+        currentVal += step;
+    }
+    
+    return range; 
+}
+
+function cracklePop() {
+    range(101,1).forEach(function(num){
+        
+        let output = ""; 
+        
+        if (num%3 == 0) {
+            output += 'Crackle'; 
+        }
+        
+        if (num%5 == 0) {
+            output += 'Pop';  
+        } else if (!output) {
+            output = num; 
+        }
+        
+        console.log(output); 
+    });
+}
+
+cracklePop(); 
+
